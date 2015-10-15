5b7aeaab287f5de241de39e9a2259511bdb21b8b
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2015-10-15 13:00:08 +0300

    Testing TRUE/FALSE @ options("popEpi.datatable") now testers.R::check_full() and not in testthat.R

--- testers.R
+++ testers.R
@@ -1,7 +1,7 @@
 
 
-check_full <- function() {
-  ## uns R CMD CHECK with all possible tests
+check_all <- function() {
+  ## runs R CMD CHECK with all possible tests; only with options("popEpi.datatable" = TRUE)
   old <- Sys.getenv("NOT_CRAN")
   Sys.setenv("NOT_CRAN" = "true")
   devtools::check(".")
@@ -9,7 +9,7 @@ check_full <- function() {
 }
 
 check_some <- function() {
-  ## runs R CMD CHECK with only the tests that CRAN will run
+  ## runs R CMD CHECK with only the tests that CRAN will run; only with options("popEpi.datatable" = TRUE)
   old <- Sys.getenv("NOT_CRAN")
   Sys.setenv("NOT_CRAN" = "false")
   devtools::check(".")
@@ -26,11 +26,29 @@ test_some <- function()  {
 }
 
 test_all <- function()  {
-  ## runs all possible tests
+  ## runs all possible tests; only with options("popEpi.datatable" = TRUE)
   old <- Sys.getenv("NOT_CRAN")
   Sys.setenv("NOT_CRAN" = "true")
   devtools::test(".")
   Sys.setenv("NOT_CRAN" = old)
+}
+
+test_full <- function() {
+  ## runs all possible tests with both TRUE/FALSE for options("popEpi.datatable")
+  old <- Sys.getenv("NOT_CRAN")
+  
+  Sys.setenv("NOT_CRAN" = "true")
+  options("popEpi.datatable" = TRUE)
+  cat("Starting checking with popEpi.datatable = TRUE \n")
+  devtools::test(".")
+  
+  cat("Finished checking with popEpi.datatable = TRUE; starting with popEpi.datatable = FALSE \n")
+  options("popEpi.datatable" = FALSE)
+  devtools::test(".")
+  
+  cat("Finished checking with popEpi.datatable = FALSE \n")
+  Sys.setenv("NOT_CRAN" = old)
+  
 }
 
 check_none <- function() {
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,17 +1,4 @@
 library(testthat)
 library(popEpi)
+test_check("popEpi")
 
-if (Sys.getenv("NOT_CRAN") == "true") {
-  
-  options("popEpi.datatable" = TRUE)
-  cat("Starting checking with popEpi.datatable = TRUE \n")
-  test_check("popEpi")
-  
-  cat("Finished checking with popEpi.datatable = TRUE; starting with popEpi.datatable = FALSE \n")
-  options("popEpi.datatable" = FALSE)
-  test_check("popEpi")
-  
-  cat("Finished checking with popEpi.datatable = FALSE \n")
-} else {
-  test_check("popEpi")
-}
