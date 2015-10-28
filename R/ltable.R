a30272a0438c6e5498785d465d82fc6f11aae022
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2015-10-28 08:19:37 +0200

    ltable & expr.by.cj no longer retain the same class as data

--- R/ltable.R
+++ R/ltable.R
@@ -156,7 +156,7 @@ ltable <- function(data,
     
   }
   
-  
+  setDT(tab)
   setkeyv(tab,by.vars)
   if (!getOption("popEpi.datatable")) setDFpe(tab)
   tab
@@ -218,6 +218,7 @@ expr.by.cj <- function(data,
   e = substitute(expr)
   tab <- data[subset, ][cj, eval(e, envir = .SD), keyby=.EACHI, .SDcols = .SDcols, ...]
   
+  setDT(tab)
   if (length(old_key) > 0) setkeyv(data, old_key)
   tab
 }
