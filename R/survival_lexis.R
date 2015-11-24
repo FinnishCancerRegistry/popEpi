7b3d85a65aa71a6ac4729b4984d0d8a170053206
Joonas Miettinen <joonas.miettinen@cancer.fi>
2015-11-24 16:21:39 +0200

    survtab_lex seems to work most of the time... not with pp stuff though

--- R/survival_lexis.R
+++ R/survival_lexis.R
@@ -2,30 +2,54 @@
 # dt[, fot := as.integer(ex_date-dg_date)/365.24]
 # s <- dt[, Surv(time = rep(0, nrow(dt)), time2 = fot, event = status %in% 1:2)]
 
-# library(Epi)
-# library(popEpi)
-# dt <- copy(sire)[dg_date < ex_date,]
-# dt[, agegr := cut(dg_age, c(0,50,75,Inf))]
-# dt <- Lexis(data = dt, entry = list(FUT = 0, AGE = dg_age, CAL = get.yrs(dg_date)),
-#             exit = list(CAL = get.yrs(ex_date)), entry.status = 0L, exit.status = status, merge = TRUE)
 
-survtab_lex <- function(data, print, adjust, breaks, pophaz, weights, ...) {
+survtab_lex <- function(data, print = NULL, adjust = NULL, breaks = NULL, pophaz = NULL, weights = NULL, event.values = NULL, surv.type = "surv.rel", ...) {
   
+  ## checks --------------------------------------------------------------------
   if (!inherits(data, "Lexis")) stop("data is not a Lexis object")
+  
   allScales <- attr(data, "time.scales")
   splitScales <- names(breaks)
+  
+  if (is.null(event.values)) {
+    event.values <- if (is.factor(data$lex.Xst)) levels(data$lex.Xst) else sort(unique(data$lex.Xst))
+  }
+  event.values <- intersect(event.values, unique(data$lex.Xst))
+  event.values <- setdiff(event.values, unique(data$lex.Cst))
+  cens.values <- setdiff(unique(data$lex.Cst), event.values)
+  if (length(cens.values) == 0) stop("could not determine which values of lex.Cst / lex.Xst imply censoring; supply event.values by hand and make sure data has meaningful lex.Cst and lex.Xst values")
+  if (length(cens.values) == 0) stop("could not determine which values of lex.Xst imply events; supply event.values by hand and make sure data has meaningful lex.Cst and lex.Xst values")
+  
+  ## ensure breaks make sense --------------------------------------------------
+  checkBreaksList(x, breaks = breaks)
+  ## match break types to time scale types
+  ## (don't try to match time scales to breaks)
+  for (k in allScales) {
+    breaks[[k]] <- matchBreakTypes(x, breaks = breaks[[k]], timeScale = k)
+  }
   
   x <- splitMulti(data, breaks = breaks, drop = TRUE, merge = TRUE)
   print <- evalPopArg(x, substitute(print), DT = TRUE)
   adjust <- evalPopArg(x, substitute(adjust), DT = TRUE)
   setcolsnull(x, keep = c("lex.id", "lex.dur", allScales, "lex.Cst", "lex.Xst", setdiff(names(pophaz), "haz")))
-  x[, names(print) := print]
-  x[, names(adjust) := adjust]
-  print <- names(print)
+  if (!is.null(print)) x[, names(print) := print]
+  if (!is.null(adjust)) x[, names(adjust) := adjust]
+  print <- names(print) ## note: names(NULL) equals NULL
   adjust <- names(adjust)
   
-  x <- cutLowMerge(x, pophaz, by = setdiff(names(pophaz), "haz"), 
-                   mid.scales = intersect(names(pophaz), allScales))
+  
+#   ## date time scales? ---------------------------------------------------------
+#   ## this actually needs to also handle breaks in an intellgent way!
+#   areDates <- x[, sapply(.SD, is.Date), .SDcols = allScales]
+#   areDifftimes <- x[, sapply(.SD, function(x) inherits(x, "difftime") && units(x) == "days"), .SDcols = allScales]
+#   if (any(areDates)) x[, (allScales[areDates]) := lapply(.SD, get.yrs, year.length = "actual"), .SDcols = allScales[areDates]]
+#   if (any(areDifftimes)) x[, (allScales[areDifftimes]) := lapply(.SD, function(x) x/365.242199), .SDcols = allScales[areDifftimes]]
+  
+  if (!is.null(pophaz)) {
+    x <- cutLowMerge(x, pophaz, by = setdiff(names(pophaz), "haz"), 
+                     mid.scales = intersect(names(pophaz), allScales))
+  }
+ 
   setattr(x, "breaks", breaks)
   setattr(x, "class", c("Lexis", "data.table", "data.frame"))
   
@@ -34,20 +58,32 @@ survtab_lex <- function(data, print, adjust, breaks, pophaz, weights, ...) {
   ## still need to compute pp-weighted figures below. they all have to be done
   ## on the level of the splitted observations!
   # c("d.pp", "d.exp.pp", "d.pp.2",if (surv.method == "hazard") "pyrs.pp" else "n.eff.pp") else NULL)
-  x <- laggre(x, aggre = c(print, adjust, names(breaks)[1]), verbose = FALSE,
-              expr = list(d.exp = sum(haz*lex.dur)))
   
+  x <- laggre(x, aggre = c(print, adjust, names(breaks)[1]), verbose = FALSE,
+              expr = if (surv.type %in% c("surv.rel", "cif.rel") && "haz" %in% names(x)) list(d.exp = sum(haz*lex.dur)) else NULL)
+  
+  dn <- CJ(C = cens.values, X = event.values)
+  dn <- paste0("from",dn$C, "to", dn$X)
+  dn <- intersect(dn, names(x))
   
   st <- survtab_ag(x, surv.scale = names(breaks)[1L], adjust = adjust,
-                   print = print, weights = weights, surv.type = "surv.rel",
-                   d.exp = d.exp)
+                   d = dn, pyrs = pyrs, 
+                   print = print, weights = weights, surv.type = surv.type,
+                   d.exp = if (surv.type %in% c("surv.rel", "cif.rel")) "d.exp" else NULL)
   
 }
+# library(Epi)
+# library(popEpi)
+# dt <- copy(sire)[dg_date < ex_date,]
+# dt[, agegr := cut(dg_age, c(0,50,75,Inf))]
+# dt <- Lexis(data = dt, entry = list(FUT = 0, AGE = dg_age, CAL = get.yrs(dg_date)),
+#             exit = list(CAL = get.yrs(ex_date)), entry.status = 0L, exit.status = status, merge = TRUE)
 # pm <- copy(popEpi::popmort)
 # setnames(pm, c("agegroup", "year"), c("AGE", "CAL"))
-# st <- survtab_lex(dt, print = "sex", adjust = "agegr", 
-#                   pophaz = pm,
-#                   weights = c(0.2,0.4,0.4),
+# st <- survtab_lex(dt, print = NULL, #adjust = "agegr", 
+#                   # pophaz = pm,
+#                   surv.type = "surv.obs",
+#                   # weights = list(agegr = c(0.2,0.4,0.4)),
 #                   breaks = list(FUT = seq(0,5,1/12)))
 
 
