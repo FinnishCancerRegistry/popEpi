3219579e1311d669f1faab269451c169c39ccc64
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2015-12-01 14:54:35 +0200

    updated survmean tests

--- tests/testthat/test_mean_survival.R
+++ tests/testthat/test_mean_survival.R
@@ -8,6 +8,8 @@ test_that("meansurv results the same as before", {
   ## Journal of clinical epidemiology 62.10 (2009): 1095-1102.
   ## 
   ## age-group-specific estimates differed but randomly 
+  ## (since git ref e59d5c7 extremely small differences)
+  
   sr <- copy(sire)[dg_date < ex_date, ]
   sr$agegr <- cut(sr$dg_age, c(0,45,60,Inf), right=FALSE)
   x <- lexpand(sr, birth  = bi_date, entry = dg_date, exit = ex_date,
@@ -16,7 +18,10 @@ test_that("meansurv results the same as before", {
   sma <- survmean(x, pophaz=popmort, by.vars="agegr", 
                   ext.breaks = list(fot = c(0:6/12, 0.75, 1:100), age = c(0, 125)))
   
-  expect_equal(sma$est, c(34.368997, 21.589844, 7.895499), tol = 0.005, scale = 1)
+  ## values to test against computed on 2015-12-01;
+  ## git ref: e59d5c7
+  expect_equal(sma$est, c(34.369009,  21.589894, 7.901299), tol = 0.005, scale = 1)
+  expect_equal(sma$exp, c(45.82442, 31.19623, 13.57836), tol = 0.005, scale = 1)
   
 })
 
