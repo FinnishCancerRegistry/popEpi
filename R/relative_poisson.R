78549282cdd986549cbd6fb53d3c8d4442c31926
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2016-01-13 22:45:51 +0200

    added relpois_ag

--- R/relative_poisson.R
+++ R/relative_poisson.R
@@ -244,3 +244,166 @@ relpois <- function(data,
 
 
 
+#' @title Excess hazard Poisson model
+#' @author Joonas Miettinen, Karri Seppa
+#' @description Estimate a Poisson Piecewise Constant Excess
+#' Hazards Model
+#' @param formula a formula with the counts of events as the response.
+#' Passed on to \code{glm}. May contain usage of the \code{offset()} function
+#' instead of supplying the offset for the Poisson model via the argument
+#' \code{offset}.
+#' @param data an \code{aggre} object (an aggregated data set; 
+#' see \code{\link{as.aggre}} and \code{\link{aggre}})
+#' @param d.exp the counts of expected cases. Mandatory.
+#' E.g. \code{d.exp = EXC_CASES}, where \code{EXC_CASES} is a column in data.
+#' @param offset the offset for the Poisson model, supplied as e.g.
+#' \code{offset = log(PTIME)}, where \code{PTIME} is a subject-time
+#' variable in data. Not mandatory, but almost always should be supplied.
+#' @param breaks optional; a numeric vector of [a,b) breaks to specify
+#' survival intervals over the follow-up time; if \code{NULL}, the 
+#' existing breaks along the mandatory time scale mentioned in \code{formula}
+#' are used
+#' @param subset a logical vector or condition; e.g. \code{subset = sex == 1};
+#' limits the data before estimation
+#' @param piecewise \code{logical}; if \code{TRUE}, and if any time scale
+#' from data is used (mentioned) in the formula, the time scale is 
+#' transformed into a factor variable indicating intervals on the time scale.
+#' Otherwise the time scale left as it is, usually a numeric variable.
+#' E.g. if \code{formula = counts ~ TS1*VAR1}, \code{TS1} is transformed
+#' into a factor before fitting model.
+#' @param ... any other argument passed on to \code{\link[stats]{glm}} such as 
+#' \code{control} or \code{weights}
+#' @import stats
+#' 
+#' @return
+#' A \code{relpois} object created using a custom Poisson family construct.
+#' 
+#' @seealso
+#' \code{\link{lexpand}}, \code{\link{poisson}}, \code{\link{glm}}
+#' 
+#' @examples
+#' ## use the simulated rectal cancer cohort
+#' sr <- copy(sire)
+#' sr$agegr <- cut(sr$dg_age, c(0,45,60,Inf), right=FALSE)
+#' 
+#' ## usable straight away after splitting
+#' fb <- c(0,3/12,6/12,1,2,3,4,5)
+#' x <- lexpand(sr, birth = bi_date, entry = dg_date,
+#'              exit = ex_date, status=status %in% 1:2,
+#'              breaks = list(fot=fb), 
+#'              pophaz=popmort, pp = FALSE,
+#'              aggre = list(agegr, fot))
+#'              
+#' rpm <- relpois_ag(formula = from0to1 ~ fot + agegr,  data = x,
+#'                   d.exp = d.exp, offset = log(pyrs))
+#' summary(rpm)
+#'  
+#' ## the usual functions for handling glm models work
+#' rpm2 <- update(rpm, . ~ fot*agegr)
+#' anova(rpm, rpm2, test="LRT")
+#' AIC(rpm, rpm2)
+#' @export
+
+relpois_ag <- function(formula, data, d.exp, offset = NULL, breaks = NULL, subset = NULL, piecewise = TRUE, ...) {
+  
+  TF <- environment()
+  PF <- parent.frame(1L)
+  original_formula <- formula
+  
+  if (!inherits(data, "aggre")) stop("data is not an aggre object. Please aggregate your data first using e.g. lexpand(). If your data is aggregated, use as.aggre() to mark it as such.")
+  
+  formula <- evalRecursive(formula, env = TF, enc = PF)$arg
+  if (missing(formula) || !inherits(formula, "formula")) stop("formula not defined")
+  
+  ## detect survival time scale ------------------------------------------------
+  oldBreaks <- attr(data, "breaks")
+  if (is.null(oldBreaks)) stop("data does not have breaks information. Is it a result of using aggre() or as.aggre()?")
+  survScale <- intersect(all.vars(formula), names(oldBreaks))
+  if (length(survScale) > 1L) stop("Found several used time scales in formula, which is not supported (found ", paste0("'", survScale, "'", collapse = ", "), ")")
+  
+  ## pre-find args -------------------------------------------------------------
+  sub_d.exp <- evalRecursive(substitute(d.exp), env = data[1L, ], enc = PF)$argSub
+  sub_offset <- evalRecursive(substitute(offset), env = data[1L, ], enc = PF)$argSub
+  
+  ## prep & subset data --------------------------------------------------------
+  subset <- substitute(subset)
+  subset <- evalLogicalSubset(data, subset)
+  
+  av <- c(all.vars(formula), all.vars(sub_d.exp), all.vars(sub_offset))
+  av <- intersect(names(data), av)
+  
+  x <- subsetDTorDF(data, subset = subset, select = av)
+  
+  setDT(x)
+  setattr(x, "class", c("aggre", "data.table", "data.frame"))
+  
+  ## handle breaks -------------------------------------------------------------
+  if (!is.null(breaks)) {
+    if (!piecewise) stop("Supplied breaks but piecewise = FALSE. Please select piecewise = TRUE if you want piecewise estimates defined by the breaks.")
+    breaks <- sort(unique(breaks))
+    if (!all(breaks %in% oldBreaks[[survScale]])) stop("Supplied breaks are not a subset of the breaks in data. See the breaks in data using e.g. attr(data, 'breaks')")
+    
+  } else breaks <- oldBreaks[[survScale]]
+  
+  if (piecewise && length(survScale) > 0L) {
+    breaks <- breaks - .Machine$double.eps^0.5 ## ensures cutting correctness
+    
+    set(x, j = survScale, value = cut(x[[survScale]], breaks = breaks, right = FALSE, labels = FALSE))
+    
+    breaks <- round(breaks, 2L)
+    pieces <- paste0("[", breaks[-length(breaks)], ", ", breaks[-1L], ")")
+    set(x, j = survScale, value = pieces[x[[survScale]]])
+    
+  }
+  
+  ## eval value args -----------------------------------------------------------
+  d.exp <- evalPopArg(x, sub_d.exp, enclos = PF, 
+                      DT = TRUE, recursive = TRUE)
+  if (is.null(d.exp)) stop("argument d.exp was not supplied")
+  d.exp <- rowSums(d.exp)
+  if (length(d.exp) == nrow(data)) d.exp <- d.exp[subset]
+  
+  offset <- evalPopArg(x, sub_offset, enclos = PF, 
+                       DT = TRUE, recursive = TRUE)
+  if (!is.null(offset)) offset <- rowSums(offset)
+  if (length(offset) == nrow(data)) offset <- offset[subset]
+  
+  
+  ## custom poisson family -----------------------------------------------------
+  RPL <- copy(poisson())
+  RPL$link <- "glm relative survival model with Poisson error"
+  RPL$linkfun <- function(mu, d.exp = TF$d.exp) {
+    log(mu - d.exp)
+  }
+  RPL$linkinv <- function(eta, d.exp = TF$d.exp) {
+    d.exp + exp(eta)
+  }
+  
+  
+  init_maker <- quote( {
+    if (any(y < 0)) stop(paste("Negative values not allowed for", 
+                               "the Poisson family"))
+    n <- rep.int(1, nobs)
+    mustart <- pmax(y, rep(1,times=length(y))) + 0.1
+  } )
+  
+  RPL$initialize <- init_maker
+  
+  
+  ## glm call ------------------------------------------------------------------
+  
+  ## NOTE: parent.frame(3L) to find this (this function's) environment
+  ml <- glm(formula = formula, data=x, offset = parent.frame(3L)$offset, family = RPL, ...)
+  
+  ## final touches -------------------------------------------------------------
+  
+  ml$call <- match.call()
+  setattr(ml, "class", c("relpois", "glm", "lm"))
+  
+  ml
+}
+
+
+
+
+
