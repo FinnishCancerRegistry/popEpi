bfc1553627af0f3e87ed22ddb6ff001610b52e57
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2016-02-29 23:15:29 +0200

    test survmean_lex results against its own results

--- tests/testthat/test_mean_survival.R
+++ tests/testthat/test_mean_survival.R
@@ -26,14 +26,6 @@ test_that("survmean results the same as before", {
 })
 
 test_that("survmean_lex() agrees with old results", {
-  ## note: used to check against the results of
-  ## Seppa, Karri, and Timo Hakulinen. 
-  ## "Mean and median survival times of cancer patients should be corrected 
-  ## for informative censoring." 
-  ## Journal of clinical epidemiology 62.10 (2009): 1095-1102.
-  ## 
-  ## age-group-specific estimates differed but randomly 
-  ## (since git ref e59d5c7 quite small differences)
   
   library(Epi)
   library(survival)
@@ -55,10 +47,10 @@ test_that("survmean_lex() agrees with old results", {
                      pophaz = pm, data = x,
                      breaks = list(FUT = seq(0, 10, 1/12)))
   
-  ## values to test against computed on 2015-12-01;
-  ## git ref: e59d5c7
-  expect_equal(sm$est, c(34.369009,  21.589894, 7.901299), tol = 0.005, scale = 1)
-  expect_equal(sm$exp, c(45.82442, 31.19623, 13.57836), tol = 0.005, scale = 1)
+  ## values to test against computed on 2015-02-29;
+  ## git ref: 8b50a33a52cc98defaa5ddc8e8fc226288b832d1
+  expect_equal(sm$est, c(34.089451,  21.544842, 7.916737), tol = 0.0005, scale = 1)
+  expect_equal(sm$exp, c(34.50633, 22.39038, 11.00145), tol = 0.0005, scale = 1)
   
   
 })
