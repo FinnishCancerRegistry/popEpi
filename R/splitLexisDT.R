8e7f762d1b3a21be887d30b242fd19b070392063
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2016-03-08 14:46:32 +0200

    splitLexisDT gains example with Date variables

--- R/splitLexisDT.R
+++ R/splitLexisDT.R
@@ -32,16 +32,33 @@
 #' @export splitLexisDT
 #' @examples
 #' library(Epi)
-#' x <- Lexis(data=sire[1000:1100], 
+#' data(sire)
+#' x <- Lexis(data=sire[1000:1100, ], 
 #'            entry = list(fot=0, per=get.yrs(dg_date), age=dg_age), 
 #'            exit=list(per=get.yrs(ex_date)), exit.status=status)
 #' BL <- list(fot=seq(0, 5, by = 3/12), per=c(2008, 2013))
+#' 
 #' x2 <- splitMulti(x, breaks = BL, drop = FALSE)
+#' 
 #' x3 <- splitLexisDT(x, breaks = BL$fot, timeScale = "fot", drop = FALSE)
 #' x3 <- splitLexisDT(x3, breaks = BL$per, timeScale = "per", drop = FALSE)
+#' 
 #' x4 <- splitLexis(x,  breaks = BL$fot, time.scale = "fot")
 #' x4 <- splitLexis(x4, breaks = BL$per, time.scale = "per")
 #' ## all produce identical results
+#' 
+#' ## using Date variables
+#' x <- Lexis(data=sire[1000:1100, ], 
+#'            entry = list(fot=0, per=dg_date, age=dg_date-bi_date), 
+#'            exit=list(per=ex_date), exit.status=status)
+#' BL <- list(fot = 0:5*365.25, per = as.Date(c("2008-01-01", "2013-01-01")))
+#' 
+#' x2 <- splitMulti(x, breaks = BL, drop = FALSE)
+#' 
+#' x3 <- splitLexisDT(x, breaks = BL$fot, timeScale = "fot", drop = FALSE)
+#' x3 <- splitLexisDT(x3, breaks = BL$per, timeScale = "per", drop = FALSE)
+#' 
+#' ## splitLexis may not work
 splitLexisDT <- function(lex, breaks, timeScale, merge = TRUE, drop = TRUE) {
   
   TF <- environment()
@@ -61,7 +78,7 @@ splitLexisDT <- function(lex, breaks, timeScale, merge = TRUE, drop = TRUE) {
   ## lexVars: if !merge, will drop all but these (NOTE: checkLexisData
   ## check for existence of these)
   lexVars <- c("lex.id", "lex.multi", allScales, "lex.dur", "lex.Cst", "lex.Xst")
-  lexVars <- intersect(names(lex), lexVars)
+  lexVars <- intersect(lexVars, names(lex))
   othVars <- setdiff(names(lex), lexVars)
   
   ## remove any existing breaks already split by;
@@ -177,9 +194,13 @@ splitLexisDT <- function(lex, breaks, timeScale, merge = TRUE, drop = TRUE) {
     
   }
   
-  ## final touch & attributes --------------------------------------------------
+  ## harmonize time scales -----------------------------------------------------
+  ## numeric time scales are forced to the lowest common denominator:
+  ## difftime -> integer -> double (though difftime is not numeric class)
+  harmonizeNumericTimeScales(l, times = c(allScales, "lex.dur"))
   
-  setcolorder(l, neworder = intersect(c(lexVars, othVars), names(l)))
+  ## final touch & attributes --------------------------------------------------
+  setcolorder(l, neworder = intersect(c(lexVars, othVars), names(l))) #merge=T/F
   if (!drop) breaks <- unprotectFromDrop(breaks)
   allBreaks[[timeScale]] <- sort(unique(c(allBreaks[[timeScale]], breaks)))
   setattr(l, "breaks", allBreaks)
