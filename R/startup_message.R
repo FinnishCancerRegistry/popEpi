742b2d9c18afe40a4f7c4091707886ca8224ee87
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2016-04-26 10:23:52 +0300

    option "popEpi.datatable" now se on _load_ instead of attach

--- R/startup_message.R
+++ R/startup_message.R
@@ -1,15 +1,23 @@
 
 .onAttach <- function(...) {
   if (interactive()) {
-    packageStartupMessage("Using popEpi. See news(package='popEpi') for changes.")
-    packageStartupMessage("popEpi's appropriate data outputs are in data.table (enhanced data.frame) format by default;")
-    packageStartupMessage("see ?popEpi for changing this.")
-    
+    msg <- paste0("Using popEpi. See news(package='popEpi') for changes. \n",
+                  "popEpi's appropriate data outputs are in data.table",
+                  "(enhanced data.frame) format by default; \n",
+                  "see ?popEpi for changing this.")
+    packageStartupMessage(msg)
+  }
+}
+
+.onLoad <- function(...) {
+  opt <- getOption("popEpi.datatable")
+  if (!is.null(opt) && is.logical(opt) && !isTRUE(opt)) {
+    warning("Option 'popEpi.datatable' was set to TRUE when loading popEpi.", 
+            call. = FALSE)
   }
   options("popEpi.datatable" = TRUE)
   
 }
-
 
 
 
