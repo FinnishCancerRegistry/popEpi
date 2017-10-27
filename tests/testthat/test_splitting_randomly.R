e3ea6dc4180d027089ef5c733445f3fd79466998
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2017-10-27 12:41:55 +0300

    splitting: oops, uncommented fixed data + random splitting tests

--- tests/testthat/test_splitting_randomly.R
+++ tests/testthat/test_splitting_randomly.R
@@ -4,95 +4,95 @@ context(
 
 
 
-# test_that("splitting funs congruent with random splitting and fixed data", {
-#   
-#   skip_on_cran()
-#   skip_on_travis()
-#   skip_on_appveyor()
-#   
-#   library("Epi")
-#   library("data.table")
-#   
-#   data("occup", package = "Epi")
-#   
-#   occup <- Epi::Lexis(
-#     entry = list(age = AoE, per = DoE),
-#     exit = list(per = DoX),
-#     entry.status = 0L,
-#     exit.status = as.integer(Xst == "D"),
-#     data = occup
-#   )
-#   
-#   sire <- setDT(copy(popEpi::sire))
-#   sire[, "dg_yrs" := get.yrs(dg_date, "actual")]
-#   sire[, "ex_yrs" := get.yrs(ex_date, "actual")]
-#   sire[, "bi_yrs" := get.yrs(bi_date, "actual")]
-#   sire[, "id":= 1:.N]
-#   sire <- Lexis(
-#     data=sire[dg_date < ex_date], 
-#     entry=list(fot=0, per=dg_yrs, age=dg_age),
-#     exit=list(per=ex_yrs), 
-#     merge=TRUE, 
-#     exit.status = status, 
-#     entry.status = 0L
-#   )
-#   
-#   
-#   sibr <- setDT(copy(popEpi::sibr))
-#   sibr[, "dg_yrs" := get.yrs(dg_date, "actual")]
-#   sibr[, "ex_yrs" := get.yrs(ex_date, "actual")]
-#   sibr[, "bi_yrs" := get.yrs(bi_date, "actual")]
-#   sibr[, "id":= 1:.N]
-#   sibr <- Lexis(
-#     data=sibr[dg_date < ex_date], 
-#     entry=list(fot=0, per=dg_yrs, age=dg_age),
-#     exit=list(per=ex_yrs), 
-#     merge=TRUE, 
-#     exit.status = status, 
-#     entry.status = 0L
-#   )
-#   
-#   lex_list <- list(
-#     occup = occup,
-#     sire = sire,
-#     sibr = sibr
-#   )
-#   
-#   n_random_splits <- 500
-#   invisible(lapply(names(lex_list), function(lex_nm) {
-#     lapply(1:n_random_splits, function(i) {
-#       used_seed <- get_random_seed()
-#       set.seed(used_seed)
-#       l <- random_splitting_on(lex = lex, n.max.breaks = 50)
-#       # list contents in order: Epi::splitLexis, splitLexisDT, splitMulti
-#       
-#       lapply(2:length(l), function(list_pos) {
-#         
-#         tt_msg <- paste0(
-#           "Epi::splitLexis and ", c("splitLexisDT", "splitMulti")[list_pos-1],
-#           " are in agreement in data '", lex_nm, "' using seed ", used_seed
-#         )
-#         
-#         test_that(tt_msg, {
-#           expect_equal(l[[1]], l[[list_pos]], check.attributes = FALSE)
-#         })
-#         
-#       })
-#       
-#       test_that(
-#         paste0(
-#           "splitLexisDT and splitMulti are in agreement in data '", lex_nm, 
-#           "' using seed ", used_seed
-#         ),
-#         expect_equal(l[[2]], l[[3]], check.attributes = TRUE)
-#       )
-#       
-#     })
-#   }))
-#   
-#   
-#   
-# })
+test_that("splitting funs congruent with random splitting and fixed data", {
+
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
+  library("Epi")
+  library("data.table")
+
+  data("occup", package = "Epi")
+
+  occup <- Epi::Lexis(
+    entry = list(age = AoE, per = DoE),
+    exit = list(per = DoX),
+    entry.status = 0L,
+    exit.status = as.integer(Xst == "D"),
+    data = occup
+  )
+
+  sire <- setDT(copy(popEpi::sire))
+  sire[, "dg_yrs" := get.yrs(dg_date, "actual")]
+  sire[, "ex_yrs" := get.yrs(ex_date, "actual")]
+  sire[, "bi_yrs" := get.yrs(bi_date, "actual")]
+  sire[, "id":= 1:.N]
+  sire <- Lexis(
+    data=sire[dg_date < ex_date],
+    entry=list(fot=0, per=dg_yrs, age=dg_age),
+    exit=list(per=ex_yrs),
+    merge=TRUE,
+    exit.status = status,
+    entry.status = 0L
+  )
+
+
+  sibr <- setDT(copy(popEpi::sibr))
+  sibr[, "dg_yrs" := get.yrs(dg_date, "actual")]
+  sibr[, "ex_yrs" := get.yrs(ex_date, "actual")]
+  sibr[, "bi_yrs" := get.yrs(bi_date, "actual")]
+  sibr[, "id":= 1:.N]
+  sibr <- Lexis(
+    data=sibr[dg_date < ex_date],
+    entry=list(fot=0, per=dg_yrs, age=dg_age),
+    exit=list(per=ex_yrs),
+    merge=TRUE,
+    exit.status = status,
+    entry.status = 0L
+  )
+
+  lex_list <- list(
+    occup = occup,
+    sire = sire,
+    sibr = sibr
+  )
+
+  n_random_splits <- 500
+  invisible(lapply(names(lex_list), function(lex_nm) {
+    lapply(1:n_random_splits, function(i) {
+      used_seed <- get_random_seed()
+      set.seed(used_seed)
+      l <- random_splitting_on(lex = lex, n.max.breaks = 50)
+      # list contents in order: Epi::splitLexis, splitLexisDT, splitMulti
+
+      lapply(2:length(l), function(list_pos) {
+
+        tt_msg <- paste0(
+          "Epi::splitLexis and ", c("splitLexisDT", "splitMulti")[list_pos-1],
+          " are in agreement in data '", lex_nm, "' using seed ", used_seed
+        )
+
+        test_that(tt_msg, {
+          expect_equal(l[[1]], l[[list_pos]], check.attributes = FALSE)
+        })
+
+      })
+
+      test_that(
+        paste0(
+          "splitLexisDT and splitMulti are in agreement in data '", lex_nm,
+          "' using seed ", used_seed
+        ),
+        expect_equal(l[[2]], l[[3]], check.attributes = TRUE)
+      )
+
+    })
+  }))
+
+
+
+})
 
 
 
