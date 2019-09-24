969344dcff1721cf1c47253e96acb2693d300ef1
Joonas Miettinen <joonas.miettinen@cancer.fi>
2019-09-24 12:25:09 +0000

    Surv unit test fixed

--- tests/testthat/test_Surv.R
+++ tests/testthat/test_Surv.R
@@ -12,9 +12,6 @@ testthat::test_that("survival::Surv and popEpi::Surv's formals are in harmony",{
   
   popepi_formals <- as.list(formals(popEpi::Surv))
   survival_formals <- as.list(formals(survival::Surv))
-  if (!"id" %in% names(survival_formals)) {
-    popepi_formals["id"] <- NULL
-  }
   testthat::expect_identical(
     object = popepi_formals,
     expected = survival_formals
