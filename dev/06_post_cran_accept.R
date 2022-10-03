6b03392a1b30f4104f33946a7ef2512443036f31
Joonas Miettinen <joonas.miettinen@cancer.fi>
2022-10-03 10:52:58 +0300

    build: fix dev/06.R

--- dev/06_post_cran_accept.R
+++ dev/06_post_cran_accept.R
@@ -5,6 +5,10 @@ lines <- readLines("CRAN-SUBMISSION")
 cran_version <- sub("Version: ", "", lines[1])
 git_ref <- sub("SHA: ", "", lines[3])
 
-cmd <- paste0("git tag ", cran_version, " ", git_ref)
+cmd1 <- paste0("git tag ", cran_version, " ", git_ref)
 message("run this cmd in system command line:")
-message(cmd)
+message(cmd1)
+
+cmd2 <- paste0("git push --tags")
+message("run this cmd in system command line:")
+message(cmd2)
