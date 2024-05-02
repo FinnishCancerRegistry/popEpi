41338021d03c74960e7970949234b4495db748c8
Joonas Miettinen <joonas.miettinen@cancer.fi>
2024-05-02 13:42:18 +0300

    build: refactor dev/01_misc.R; expand dev/06_release.R

--- dev/06_release.R
+++ dev/06_release.R
@@ -1,5 +1,5 @@
-# the call below requires interaction. follow the instructions 
-# to release to CRAN.
-library("devtools")
+source("https://gist.githubusercontent.com/WetRobot/a741c9d2a22fbfb52784369be8354f15/raw/r_pkg_release.R")
 
+# this requires interaction. follow the instructions
+# to release to CRAN.
 devtools::release()
