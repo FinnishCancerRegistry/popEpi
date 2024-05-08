2c4de2a4497bf8aedc41c2db6f87446e08572f78
Joonas Miettinen <joonas.miettinen@cancer.fi>
2024-05-08 09:49:41 +0300

    build: fix CRAN / github release logic

--- dev/06_release.R
+++ dev/06_release.R
@@ -1,5 +1,3 @@
-source("https://gist.githubusercontent.com/WetRobot/a741c9d2a22fbfb52784369be8354f15/raw/r_pkg_release.R")
-
 # this requires interaction. follow the instructions
 # to release to CRAN.
 devtools::release()
