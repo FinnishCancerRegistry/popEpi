a227020f6c9794b2e405aa018f1eff3bb4d7566a
Joonas Miettinen <joonas.miettinen@cancer.fi>
2024-05-08 10:27:05 +0300

    build: improve dev/05_final_touches.R

--- dev/05_final_touches.R
+++ dev/05_final_touches.R
@@ -1,14 +1,25 @@
 # all of these must be OK before CRAN release.
 library("devtools")
+du <- new.env()
+source("dev/utils.R", local = du)
 
 ## spelling --------------------------------------------------------------------
 devtools::spell_check()
 
-## manual checks ---------------------------------------------------------------
-# have you updated NEWS, NEWS.Rmd -> NEWS.md?
-
-# have you changed the version number?
-
-# is the author list up-to-date?
-
-# is cran-comments.md up-to-date?
+## README / NEWS ---------------------------------------------------------------
+if (du$ask_yn("Is README.Rmd up-to-date?")) {
+  du$git_commit_if_changes_made(
+    rmarkdown::render("README.Rmd"),
+    message = "docs: render README.Rmd"
+  )
+}
+if (!du$ask_yn("Have you bumped the version number?")) {
+  stop("bump it manually in DESCRIPTION")
+}
+if (!du$ask_yn("Do you have a news item in NEWS.md for the release?")) {
+  stop("fix NEWS.md by adding a codedoc comment block near where you made ",
+       "changes in the R scripts.")
+}
+if (!du$ask_yn("Have updated cran-comments.md?")) {
+  stop("fix cran-comments.md")
+}
--- dev/utils.R
+++ dev/utils.R
@@ -243,3 +243,56 @@ read_winbuilder_results <- function(url) {
   readLines(url, encoding = "UTF-8")
 }
 
+ask_yn <- function(
+  q,
+  yes_msg = NULL,
+  no_msg = NULL
+) {
+  message(q)
+  a <- ""
+  while (!a %in% c("y", "n")) {
+    a <- tolower(readline(": "))
+  }
+  answered_yes <- identical(a, "y")
+  if (answered_yes && !is.null(yes_msg)) {
+    message(yes_msg)
+  } else if (!answered_yes && !is.null(no_msg)) {
+    message(no_msg)
+  }
+  return(answered_yes)
+}
+
+git_exe_cmd <- function(args, system2.arg.list = NULL) {
+  stdout_file_path <- tempfile()
+  stderr_file_path <- tempfile()
+  override_arg_list <- list(command = "git", args = args,
+                            stdout = stdout_file_path,
+                            stderr = stderr_file_path)
+  arg_list <- as.list(system2.arg.list)
+  arg_list[names(override_arg_list)] <- override_arg_list
+  status <- do.call(system2, arg_list, quote = TRUE)
+  stdout <- tryCatch(readLines(stdout_file_path), error = function(e) e)
+  stderr <- tryCatch(readLines(stderr_file_path), error = function(e) e)
+  return(list(status = status, stdout = stdout, stderr = stderr))
+}
+
+git_commit_if_changes_made <- function(
+  expr,
+  message
+) {
+  stopifnot(
+    is.character(message),
+    !is.na(message),
+    length(message) == 1
+  )
+  s1 <- git_exe_cmd("status")
+  stopifnot(
+    any(grepl("nothing to commit, working tree clean", s1[["stdout"]]))
+  )
+  out <- expr # lazy eval triggered
+  s2 <- git_exe_cmd("status")
+  if (!identical(s1, s2)) {
+    git_exe_cmd(c("commit", paste0(message, collapse = " ")))
+  }
+  return(out)
+}
