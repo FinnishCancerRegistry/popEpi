5cf03a297ea64cee55e16d7ed5a607dbc93c8c0e
Joonas Miettinen <joonas.miettinen@cancer.fi>
2026-02-19 13:36:15 +0200

    docs: improve surv_lexis docs

--- R/wip_surv_lexis.R
+++ R/wip_surv_lexis.R
@@ -48,6 +48,19 @@ surv_aggre_expression_list__ <- list(
     sum((lex.Cst == x & lex.Xst == y) * iw)
   )
 )
+surv_aggre_expression_table__ <- function() {
+  dt <- data.table::data.table(
+    "Name" = surv_estimate_expression_table_clean__(
+      names(surv_aggre_expression_list__)
+    ),
+    "Expression" = vapply(
+      surv_aggre_expression_list__,
+      surv_estimate_expression_table_clean__,
+      character(1L)
+    )
+  )
+  return(dt)
+}
 surv_aggre_expression__ <- function(
   estimator_dt,
   individual_weight_col_nm = NULL
@@ -58,13 +71,27 @@ surv_aggre_expression__ <- function(
     iw_replacement <- ""
   }
   aggre_expr_set <- unlist(lapply(seq_len(nrow(estimator_dt)), function(i) {
+    # @codedoc_comment_block popEpi:::surv_aggre_expression__
+    #   + Detect variables used in the estimation expressions with `[all.vars]`.
+    # @codedoc_comment_block popEpi:::surv_aggre_expression__
     est_expr_set <- estimator_dt[["expression_set"]][[i]]
     var_nm_set <- unlist(lapply(est_expr_set, all.vars))
     var_nm_set <- unique(var_nm_set)
     var_nm_set <- var_nm_set[
       var_nm_set %in% names(surv_aggre_expression_list__) |
-        grepl("^n_events_\\[.+, *.+\\]$", var_nm_set)
+        grepl("_\\[.+, *.+\\]$", var_nm_set)
     ]
+    # @codedoc_comment_block popEpi:::surv_aggre_expression__
+    #   + Detect the general name of a transition-specific variable such as
+    #     `n_events_[0, 1]` as `n_events_[x, y]`. This general name is used
+    #     to fetch the general aggregation expression
+    #     (e.g. `"n_events_[x, y]" = quote(sum(lex.Cst == x & lex.Xst == y))`)
+    #     and this definition is then made specific to the requested transition,
+    #     e.g.  `n_events_[0, 1] = quote(sum(lex.Cst == x & lex.Xst == y))`.
+    #     Of course aggregation expressions without a specific transition such
+    #     as `t_at_risk` are unaffected by this. The table of general
+    #     aggregation expressions known to `popEpi` is shown below.
+    # @codedoc_comment_block popEpi:::surv_aggre_expression__
     var_nm_set <- sub(
       "[x, y]",
       sprintf(
@@ -95,6 +122,11 @@ surv_aggre_expression__ <- function(
       aggre_expr_string_set,
       perl = TRUE
     )
+    # @codedoc_comment_block popEpi:::surv_aggre_expression__
+    #   + The general variable definitions usually have ` * iw` in them for
+    #     enabling individual weighting. But if this is not requested then
+    #     those ` * iw` parts are removed from the aggregation expressions.
+    # @codedoc_comment_block popEpi:::surv_aggre_expression__
     aggre_expr_string_set <- gsub(
       " *[*] *iw",
       iw_replacement,
@@ -104,6 +136,17 @@ surv_aggre_expression__ <- function(
       parse(text = s)[[1]]
     })
   }), recursive = FALSE, use.names = TRUE)
+  # @codedoc_comment_block popEpi:::surv_aggre_expression__
+  #   + After going through every requested estimator we have a set of
+  #     quoted expressions such as
+  #     `list(t_at_risk = quote(sum(lex.dur)), n_events = quote(sum(lex.Cst != lex.Xst)))`.
+  #     This is turned into a quoted list of expressions with `call` and we are
+  #     done, e.g.
+  #     `quote(list(t_at_risk = sum(lex.dur), n_events = sum(lex.Cst != lex.Xst)))`.
+  #   + Table of general aggregation expressions known to `popEpi`:
+  #
+  # ${paste0(knitr::kable(popEpi:::surv_aggre_expression_table__()), collapse = "\n")}
+  # @codedoc_comment_block popEpi:::surv_aggre_expression__
   aggre_expr_set[duplicated(names(aggre_expr_set))] <- NULL
   aggre_expr_set <- aggre_expr_set[order(names(aggre_expr_set))]
   out <- as.call(c(quote(list), aggre_expr_set))
@@ -160,16 +203,10 @@ surv_lexis <- function(
   #
   # Performs the following steps:
   #
-  # - Call `surv_split_merge_aggregate_by_stratum`. If `aggre_expr` is `NULL`,
-  #   it is replaced with an internally specified expressions based on
-  #   argument `estimators`. The resulting table of aggregated data is
-  #   stratified by both `aggre_by` and by any stratifying columns found in
-  #   `weights` if a `data.table` was supplied as that argument. E.g.
-  #   with `aggre_by = "sex"` and
-  #   `weights = data.table::data.table(ag = 1:3, weight = c(100, 150, 200))`,
-  #   the statistics table is stratified by both `sex` and `ag`.
-  #   With `aggre_by = "sex"` and `weights = "individual_weight"` the table is
-  #   stratified by sex and contains individually weighted statistics.
+  # - If `aggre_expr` is `NULL`,
+  #   `estimators` is analysed and `aggre_expr` will be created by `surv_lexis`
+  #   as follows:
+  # @codedoc_insert_comment_block popEpi:::surv_aggre_expression__
   # @codedoc_comment_block popEpi::surv_lexis
   estimator_dt <- handle_arg_estimators(estimators)
   if (is.null(aggre_expr)) {
@@ -178,6 +215,17 @@ surv_lexis <- function(
       individual_weight_col_nm = if (is.character(weights)) weights else NULL
     )
   }
+  # @codedoc_comment_block popEpi::surv_lexis
+  # - Call `surv_split_merge_aggregate_by_stratum`.
+  #   The resulting table of aggregated data is
+  #   stratified by both `aggre_by` and by any stratifying columns found in
+  #   `weights` if a `data.table` was supplied as that argument. E.g.
+  #   with `aggre_by = "sex"` and
+  #   `weights = data.table::data.table(ag = 1:3, weight = c(100, 150, 200))`,
+  #   the statistics table is stratified by both `sex` and `ag`.
+  #   With `aggre_by = "sex"` and `weights = "individual_weight"` the table is
+  #   stratified by sex and contains individually weighted statistics.
+  # @codedoc_comment_block popEpi::surv_lexis
   sdt <- surv_split_merge_aggregate_by_stratum(
     dt = dt,
     breaks = breaks,
@@ -200,8 +248,7 @@ surv_lexis <- function(
     )
   }
   # @codedoc_comment_block popEpi::surv_lexis
-  # - Call `surv_estimate` to produce estimates by the strata specified above.
-  #   If `weights` was `NULL` or `character` we are done.
+  # - Call `surv_estimate` and return its result.
   # @codedoc_comment_block popEpi::surv_lexis
   sdt <- surv_estimate(
     dt = sdt,
@@ -214,8 +261,7 @@ surv_lexis <- function(
     conf_methods = conf_methods
   )
   # @codedoc_comment_block return(popEpi::surv_lexis)
-  # A `data.table` of summary statistics and survival estimates with their
-  # standard errors and confidence intervals is returned.
+  # Returns a `data.table` as produced by `surv_estimate`.
   # @codedoc_comment_block return(popEpi::surv_lexis)
   return(sdt[])
 }
