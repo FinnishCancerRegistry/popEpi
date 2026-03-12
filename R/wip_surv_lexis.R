f5034d8604726b5ebb222365b089819307d995a7
Joonas Miettinen <joonas.miettinen@cancer.fi>
2026-03-12 15:29:26 +0200

    docs: add surv_lexis examples

--- R/wip_surv_lexis.R
+++ R/wip_surv_lexis.R
@@ -49,6 +49,422 @@ surv_lexis_aggre_exprs__ <- function(
 #'   "popEpi::surv_lexis",
 #'   "surv_functions"
 #' )
+#' @examples
+#'
+#' # popEpi::surv_lexis
+#' make_pm <- function() {
+#'   pm <- data.table::copy(popEpi::popmort)
+#'   data.table::setnames(
+#'     pm,
+#'     c("year", "agegroup", "haz"),
+#'     c("ts_cal", "ts_age", "h_exp")
+#'   )
+#'   data.table::setkeyv(pm, c("sex", "ts_cal", "ts_age"))
+#'   pm <- c(
+#'     list(pm),
+#'     lapply(101:110, function(age) {
+#'       sub_pm <- pm[
+#'         pm[["ts_age"]] == 100
+#'       ]
+#'       sub_pm[
+#'         j = "ts_age" := age
+#'       ]
+#'       sub_pm[]
+#'     })
+#'   )
+#'   pm <- data.table::rbindlist(pm)
+#'   data.table::setkeyv(pm, c("sex", "ts_cal", "ts_age"))
+#'   return(pm[])
+#' }
+#'
+#' make_column_icss_ag <- function(age) {
+#'   cut(
+#'     age,
+#'     breaks = c(0, 60, 70, 80, Inf),
+#'     right = FALSE,
+#'     labels = c("0-59", "60-69", "70-79", "80+")
+#'   )
+#' }
+#'
+#' make_standard_weight_dt <- function() {
+#'   return(popEpi::ICSS[
+#'     j = list(
+#'       weight = as.double(sum(.SD[["ICSS1"]]))
+#'     ),
+#'     keyby = list(
+#'       icss_ag = make_column_icss_ag(popEpi::ICSS[["age"]])
+#'     )
+#'   ][])
+#' }
+#'
+#' make_sire <- function() {
+#'   sire <- popEpi::sire
+#'   sire <- sire[
+#'     sire[["dg_date"]] < sire[["ex_date"]] &
+#'       sire[["ex_date"]] >= as.Date("1999-01-01") &
+#'       (get.yrs(sire[["ex_date"]]) - get.yrs(sire[["bi_date"]])) < 100
+#'   ]
+#'   set.seed(1337)
+#'   sire[j = "my_stratum" := sample(2L, size = .N, replace = TRUE)]
+#'   # you can also use popEpi::Lexis_dt
+#'   sire <- Epi::Lexis(
+#'     data = sire,
+#'     entry = list(
+#'       ts_cal = popEpi::get.yrs(dg_date),
+#'       ts_age = popEpi::get.yrs(dg_date) - popEpi::get.yrs(bi_date),
+#'       ts_fut = 0.0
+#'     ),
+#'     duration = popEpi::get.yrs(ex_date) - popEpi::get.yrs(dg_date),
+#'     entry.status = 0L,
+#'     exit.status = status
+#'   )
+#'   sire[["icss_ag"]] <- make_column_icss_ag(sire[["dg_age"]])
+#'   sire[["individual_weight"]] <- popEpi::surv_individual_weights(
+#'     df = sire,
+#'     standard_weight_dt = wdt
+#'   )
+#'   return(sire[])
+#' }
+#'
+#' pm <- make_pm()
+#' wdt <- make_standard_weight_dt()
+#' sire <- make_sire()
+#'
+#' # using breaks on the calendar time causes period analysis to be performed.
+#' # this is done here for demonstration purposes but of course everything works
+#' # also without ts_cal breaks.
+#' bl <- list(
+#'   ts_fut = surv_breaks_rule_based(
+#'     lexis = sire,
+#'     ts_fut_nm = "ts_fut",
+#'     breaks = seq(0, 5, 1 / 12),
+#'     mandatory_breaks = 0:5
+#'   ),
+#'   ts_cal = c(1999, 2004)
+#' )
+#'
+#' # observed survival
+#' sdt <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl,
+#'   aggre_by = "my_stratum",
+#'   aggre_ts_col_nms = "ts_fut",
+#'   subset = NULL,
+#'   estimators = "S_ch",
+#'   conf_methods = "log",
+#'   conf_lvls = 0.95
+#' )
+#' stopifnot(
+#'   inherits(sdt, "data.table"),
+#'   "my_stratum" %in% names(sdt),
+#'   c("S_ch_est", "S_ch_se", "S_ch_lo", "S_ch_hi") %in% names(sdt)
+#' )
+#'
+#' # observed survival with direct adjusting
+#' sdt_da <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl,
+#'   aggre_by = "my_stratum",
+#'   aggre_ts_col_nms = "ts_fut",
+#'   estimators = "S_ch",
+#'   weights = wdt
+#' )
+#' stopifnot(
+#'   inherits(sdt_da, "data.table"),
+#'   "my_stratum" %in% names(sdt_da),
+#'   c("S_ch_est", "S_ch_se", "S_ch_lo", "S_ch_hi") %in% names(sdt_da)
+#' )
+#'
+#' # observed survival with individual weighting
+#' sdt_iw <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl,
+#'   aggre_by = "my_stratum",
+#'   aggre_ts_col_nms = "ts_fut",
+#'   estimators = "S_ch",
+#'   weights = "individual_weight"
+#' )
+#' stopifnot(
+#'   inherits(sdt_iw, "data.table"),
+#'   "my_stratum" %in% names(sdt_iw),
+#'   c("S_ch_est", "S_ch_se", "S_ch_lo", "S_ch_hi") %in% names(sdt_iw),
+#'
+#'   # direct adjusting and individual weighting produce similar results.
+#'   # the larger the dataset, the smaller the difference between the two.
+#'   max(abs(sdt_da[["S_ch_est"]] - sdt_iw[["S_ch_est"]])) < 0.02,
+#'   max(abs(sdt_da[["S_ch_se"]] - sdt_iw[["S_ch_se"]])) < 0.001
+#' )
+#'
+#' # observed survival with direct adjusting, multiple period estimates
+#' # with the periods as strata in output
+#' bl_multi_period <- bl
+#' bl_multi_period[["ts_cal"]] <- c(bl[["ts_cal"]], max(bl[["ts_cal"]]) + 5)
+#' sdt_da <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl_multi_period,
+#'   aggre_by = "my_stratum",
+#'   aggre_ts_col_nms = c("ts_cal", "ts_fut"),
+#'   estimators = "S_ch",
+#'   weights = wdt
+#' )
+#' stopifnot(
+#'   inherits(sdt_da, "data.table"),
+#'   c("ts_cal_start", "ts_fut_start") %in% names(sdt_da),
+#'   c("S_ch_est", "S_ch_se", "S_ch_lo", "S_ch_hi") %in% names(sdt_da)
+#' )
+#'
+#' # observed survival with individual adjusting, period estimates
+#' sdt_iw <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl_multi_period,
+#'   aggre_by = "my_stratum",
+#'   aggre_ts_col_nms = c("ts_cal", "ts_fut"),
+#'   estimators = "S_ch",
+#'   weights = "individual_weight"
+#' )
+#' stopifnot(
+#'   inherits(sdt_iw, "data.table"),
+#'   c("ts_cal_start", "ts_fut_start") %in% names(sdt_iw),
+#'   c("S_ch_est", "S_ch_se", "S_ch_lo", "S_ch_hi") %in% names(sdt_iw),
+#'
+#'   # direct adjusting and individual weighting produce similar results.
+#'   # the larger the dataset, the smaller the difference between the two.
+#'   max(abs(sdt_da[["S_ch_est"]] - sdt_iw[["S_ch_est"]])) < 0.03,
+#'   max(abs(sdt_da[["S_ch_se"]] - sdt_iw[["S_ch_se"]])) < 0.006
+#' )
+#'
+#' # a few common survival time function estimates
+#' sdt <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl,
+#'   merge_dt_by = c("sex", "ts_cal", "ts_age"),
+#'   merge_dt = pm,
+#'   aggre_by = "my_stratum",
+#'   aggre_ts_col_nms = "ts_fut",
+#'   estimators = c(
+#'     "S_ch",
+#'     "RS_e2_ch",
+#'     "NS_pp_ch"
+#'   )
+#' )
+#' stopifnot(
+#'   inherits(sdt, "data.table"),
+#'   c("S_ch_est", "RS_e2_ch_est", "NS_pp_ch_est") %in% names(sdt)
+#' )
+#'
+#' # your very own estimator (the example `se` is made up)
+#' sdt <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl,
+#'   merge_dt_by = c("sex", "ts_cal", "ts_age"),
+#'   merge_dt = pm,
+#'   aggre_by = "my_stratum",
+#'   aggre_ts_col_nms = "ts_fut",
+#'   estimators = list(
+#'     my_estimator = list(
+#'       est = quote(n_events / n_events_exp_e2),
+#'       se = quote(sqrt(n_events / (n_events_exp_e2 ^ 2)))
+#'     )
+#'   ),
+#'   conf_method = "identity"
+#' )
+#' stopifnot(
+#'   c("my_estimator_est", "my_estimator_se") %in% names(sdt)
+#' )
+#'
+#' # your very own estimator with direct adjusting
+#' sdt <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl,
+#'   merge_dt_by = c("sex", "ts_cal", "ts_age"),
+#'   merge_dt = pm,
+#'   aggre_by = "my_stratum",
+#'   aggre_ts_col_nms = "ts_fut",
+#'   estimators = list(
+#'     my_estimator = list(
+#'       est = quote(n_events / n_events_exp_e2),
+#'       se = quote(sqrt(n_events / (n_events_exp_e2 ^ 2)))
+#'     )
+#'   ),
+#'   conf_method = "identity",
+#'   weights = wdt
+#' )
+#' stopifnot(
+#'   c("my_estimator_est", "my_estimator_se") %in% names(sdt)
+#' )
+#'
+#' # your very own estimator with individual weighting
+#' sdt <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl,
+#'   merge_dt_by = c("sex", "ts_cal", "ts_age"),
+#'   merge_dt = pm,
+#'   aggre_by = "my_stratum",
+#'   aggre_ts_col_nms = "ts_fut",
+#'   estimators = list(
+#'     my_estimator = list(
+#'       est = quote(n_events / n_events_exp_e2),
+#'       se = quote(sqrt(n_events / (n_events_exp_e2 ^ 2)))
+#'     )
+#'   ),
+#'   conf_method = "identity",
+#'   weights = "individual_weight"
+#' )
+#' stopifnot(
+#'   c("my_estimator_est", "my_estimator_se") %in% names(sdt)
+#' )
+#'
+#' # your very own method of confidence interval estimation
+#' sdt <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl,
+#'   aggre_ts_col_nms = "ts_fut",
+#'   estimators = c("S_ch", "S_lt"),
+#'   conf_methods = list(list(
+#'     g = quote(qnorm(theta)),
+#'     g_inv = quote(pnorm(g))
+#'   )),
+#'   conf_lvls = 0.95,
+#'   weights = "individual_weight"
+#' )
+#' stopifnot(
+#'   c("S_ch_lo", "S_ch_hi") %in% names(sdt)
+#' )
+#'
+#' # a bunch of estimators, cause-specific ones from state 0 to 1 or 2
+#' # and non-cause-specific ones also
+#' sdt <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl,
+#'   merge_dt_by = c("sex", "ts_cal", "ts_age"),
+#'   merge_dt = pm,
+#'   aggre_by = NULL,
+#'   aggre_ts_col_nms = c("ts_fut"),
+#'   subset = NULL,
+#'   estimators = c(
+#'     "h_ch_[0, 1]",
+#'     "h_ch_[0, 2]",
+#'     "H_ch_[0, 1]",
+#'     "H_ch_[0, 2]",
+#'     "F_ch_[0, 1]",
+#'     "F_ch_[0, 2]",
+#'     "S_ch_[0, 1]",
+#'     "S_ch_[0, 2]",
+#'     "S_ch_[0, 1:2]",
+#'     "H_ch",
+#'     "H_lt",
+#'     "h_ch",
+#'     "h_lt",
+#'     "F_ch",
+#'     "F_lt",
+#'     "S_ch",
+#'     "S_lt",
+#'     "NS_pp_ch",
+#'     "RF_e2_ch"
+#'   )
+#' )
+#' stopifnot(
+#'   # hazard is additive.
+#'   all.equal(
+#'     sdt[["h_ch_est"]],
+#'     sdt[["h_ch_[0, 1]_est"]] + sdt[["h_ch_[0, 2]_est"]]
+#'   ),
+#'   # cumulative hazard is additive.
+#'   all.equal(
+#'     sdt[["H_ch_est"]],
+#'     sdt[["H_ch_[0, 1]_est"]] + sdt[["H_ch_[0, 2]_est"]]
+#'   ),
+#'   # competing risks cdf is additive. in epidemiology the cdf is often called
+#'   # the cumulative incidence function.
+#'   all.equal(
+#'     sdt[["F_ch_est"]],
+#'     sdt[["F_ch_[0, 1]_est"]] + sdt[["F_ch_[0, 2]_est"]]
+#'   ),
+#'
+#'   # competing risks survival is NOT additive!
+#'   sdt[["S_ch_[0, 1]_est"]] + sdt[["S_ch_[0, 2]_est"]] > 1,
+#'
+#'   # you can define multiple states in the cause-specific transition.
+#'   # this one covers all exit statuses and is therefore the same as overall
+#'   # survival by definition.
+#'   sdt[["S_ch_[0, 1:2]_est"]] == sdt[["S_ch_est"]]#,
+#'
+#'   # the different estimation methods produce about the same results.
+#'   # abs(sdt[["S_ch_est"]] - sdt[["S_lt_est"]]) < 0.01,
+#'   # abs(sdt[["H_ch_est"]] - sdt[["H_lt_est"]]) < 0.01,
+#'
+#'   # net survival is approximately the same as cause-specific survival in this
+#'   # rather ideal dataset.
+#'   # abs(sdt[["S_ch_[0, 1]_est"]] - sdt[["NS_pp_ch_est"]]) < 0.01,
+#'   # the ederer 2 based "relative cdf" is approximately the same as
+#'   # cause-specific cdf in this rather ideal dataset.
+#'   # abs(sdt[["F_ch_[0, 1]_est"]] - sdt[["RF_e2_ch_est"]]) < 0.01
+#' )
+#'
+#' # factor status
+#' data.table::set(
+#'   x = sire,
+#'   j = c("lex.Cst", "lex.Xst"),
+#'   value = list(
+#'     factor(
+#'       x = sire[["lex.Cst"]],
+#'       levels = 0:2,
+#'       labels = c("alive", "dead from cancer", "dead from other")
+#'     ),
+#'     factor(
+#'       x = sire[["lex.Xst"]],
+#'       levels = 0:2,
+#'       labels = c("alive", "dead from cancer", "dead from other")
+#'     )
+#'   )
+#' )
+#' sdt <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl,
+#'   merge_dt_by = c("sex", "ts_cal", "ts_age"),
+#'   merge_dt = pm,
+#'   aggre_by = NULL,
+#'   aggre_ts_col_nms = c("ts_fut"),
+#'   subset = NULL,
+#'   estimators = c(
+#'     "F_ch_['alive', 'dead from cancer']",
+#'     "F_ch_['alive', 'dead from other']"
+#'   )
+#' )
+#' stopifnot(
+#'   # produces the estimates as intended. maybe these are not too pretty but
+#'   # you could use shorter factor labels such as c("a", "dc", "do").
+#'   "F_ch_['alive', 'dead from cancer']_est" %in% names(sdt)
+#' )
+#'
+#' # TRUE/FALSE status
+#' data.table::set(
+#'   x = sire,
+#'   j = c("lex.Cst", "lex.Xst"),
+#'   value = list(
+#'     sire[["lex.Cst"]] != "alive",
+#'     sire[["lex.Xst"]] != "alive"
+#'   )
+#' )
+#' sdt <- popEpi::surv_lexis(
+#'   lexis = sire,
+#'   breaks = bl,
+#'   merge_dt_by = c("sex", "ts_cal", "ts_age"),
+#'   merge_dt = pm,
+#'   aggre_by = NULL,
+#'   aggre_ts_col_nms = c("ts_fut"),
+#'   subset = NULL,
+#'   estimators = c(
+#'     "F_ch",
+#'     "F_ch_[F, T]"
+#'   )
+#' )
+#' stopifnot(
+#'   "F_ch_[F, T]_est" %in% names(sdt),
+#'
+#'   max(abs(sdt[["F_ch_[F, T]_est"]] - sdt[["F_ch_est"]])) < 1e-15
+#' )
+#'
 surv_lexis <- function(
   lexis,
   breaks,
