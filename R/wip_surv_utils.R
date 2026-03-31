15e9ae26ae19218e03905669575e43364c008148
Joonas Miettinen <joonas.miettinen@cancer.fi>
2026-03-31 10:01:14 +0300

    feat: add util surv_interpolate

--- R/wip_surv_utils.R
+++ R/wip_surv_utils.R
@@ -105,3 +105,49 @@ surv_interval <- function(
   )
   return(work_dt[])
 }
+
+surv_interpolate <- function(
+  estimates,
+  ts_fut_stops,
+  ts_fut_stop_value,
+  estimate_start_value = 1.0,
+  method = c("linear", "geometric_mean", "hazard")[1]
+) {
+  stopifnot(
+    estimate_start_value %in% 0:1,
+    method %in% c("linear", "geometric_mean", "hazard")
+  )
+  ts_fut_breaks <- c(0.0, ts_fut_stops)
+  interval_idx <- cut(
+    ts_fut_stop_value,
+    breaks = ts_fut_breaks,
+    labels = FALSE,
+    right = FALSE
+  )
+  interval_start <- ts_fut_breaks[interval_idx]
+  interval_stop <- ts_fut_breaks[interval_idx + 1L]
+  estimates <- c(estimate_start_value, estimates)
+  interval_start_estimate <- estimates[interval_idx]
+  interval_stop_estimate <- estimates[interval_idx + 1L]
+  # first we take conditional survival
+  conditional_stop_estimate <- interval_stop_estimate / interval_start_estimate
+  interval_width <- interval_stop - interval_start
+  ts_fut_stop_value_in_interval <- (ts_fut_stop_value - interval_start)
+  w <- ts_fut_stop_value_in_interval / interval_width
+  # we interpolate it.
+  # these methods in fact produce exactly the same result --- the linear
+  # interpolation.
+  out <- switch(
+    method,
+    "linear" = (1 - w) * estimate_start_value + 
+      w * conditional_stop_estimate,
+    "geometric_mean" = conditional_stop_estimate ^ w,
+    "hazard" = exp(-(
+      -log(conditional_stop_estimate) * w
+    ))
+  )
+  # then we un-conditionalise it
+  out <- out * interval_start_estimate
+  # the result is interpolated unconditional survival at ts_fut_stop_value
+  return(out)
+}
