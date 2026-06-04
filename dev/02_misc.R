f5257c1ac578a014b1625a26b55dd54f2229c996
Joonas Miettinen <joonas.miettinen@cancer.fi>
2026-06-04 10:56:10 +0300

    build: fix 02_misc.R

--- dev/02_misc.R
+++ dev/02_misc.R
@@ -1,1 +1,1 @@
-source("https://gist.githubusercontent.com/WetRobot/a741c9d2a22fbfb52784369be8354f15/raw/github_r_pkg_license_and_ci.R")
+source("https://raw.githubusercontent.com/FinnishCancerRegistry/r-package-dev-scripts/refs/heads/main/misc.R")
