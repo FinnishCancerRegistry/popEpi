ce8b01631fb0dceda910dc0e792fcb0b7d1119cd
Joonas Miettinen <joonas.miettinen@cancer.fi>
2026-06-30 12:18:58 +0300

    build: fix 06_release.R, 07_post_cran_accept.R

--- dev/06_release.R
+++ dev/06_release.R
@@ -1,3 +1,1 @@
-# this requires interaction. follow the instructions
-# to release to CRAN.
-devtools::release()
+usethis::use_release_issue()
--- dev/07_post_cran_accept.R
+++ dev/07_post_cran_accept.R
@@ -1,9 +1,13 @@
 # steps to do after CRAN has accepted the new release.
+du <- new.env()
+source("dev/utils.R", local = du)
 
 lines <- readLines("CRAN-SUBMISSION")
-
 cran_version <- sub("Version: ", "", lines[1])
 git_ref <- sub("SHA: ", "", lines[3])
-system2("git", c("checkout", git_ref))
-source("https://gist.githubusercontent.com/WetRobot/a741c9d2a22fbfb52784369be8354f15/raw/r_pkg_release.R")
-system2("git", c("checkout", "master"))
+
+du$git_exe_cmd(c("pull", "--tags", "--force"))
+du$git_exe_cmd(c("checkout", git_ref))
+du$git_exe_cmd(c("tag", sprintf("v%s", cran_version)))
+du$git_exe_cmd(c("checkout", "master"))
+du$git_exe_cmd(c("push", "--tags", "--force"))
